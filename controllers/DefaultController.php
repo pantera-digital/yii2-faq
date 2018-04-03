9d5d6cc1d0bbf9d031f9bbeba2c368d344e719db
Your Name <you@example.com>
2018-04-04 02:29:10 +1000

    module allowed only for logined users

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace pantera\faq\controllers;
 
 use pantera\faq\models\FaqCategories;
 use pantera\faq\models\FaqQuestions;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 
@@ -12,6 +13,20 @@ use yii\web\NotFoundHttpException;
  */
 class DefaultController extends Controller
 {
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ]
+        ];
+    }
 
     /**
      * @return mixed
