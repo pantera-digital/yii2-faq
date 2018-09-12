24289f074396b4a372f86e568cec4d05d97ba945
numawsm <numkms@gmail.com>
2018-09-12 17:01:40 +1000

    меню

--- admin/Module.php
+++ admin/Module.php
@@ -4,6 +4,17 @@ namespace pantera\faq\admin;
 
 class Module extends \yii\base\Module
 {
-
+    public function getMenuItems() {
+        return [
+            [
+                'label' => 'FAQ Категории',
+                'url' => ['/faq/faq-categories/index'],
+            ],
+            [
+                'label' => 'FAQ Вопросы',
+                'url' => ['/faq/faq-questions/index'],
+            ],
+        ];
+     }
     
 }
